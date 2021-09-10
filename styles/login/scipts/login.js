344ab23b079ea3d488e179864e6917167b3f4504
Killershark77 <franciscotrianto@gmail.com>
2021-09-10 16:44:13 +0700

    Pop up alert

--- styles/login/scipts/login.js
+++ styles/login/scipts/login.js
@@ -1,22 +1,47 @@
-var button = document.getElementById("verifbtn");
 var box = document.getElementById("verifbox");
-var span = document.getElementById("");
 
-button.onclick = function(){
-    box.style.display = "flex";
+function alertbuatan(isi){
+    document.getElementById("text").innerHTML = isi;
+    document.getElementById("alertbuatan").style.display = "flex";
+}
+
+function close(){
+        document.getElementById("alertbuatan").style.display = "none";
+}
+
+function checkisi(){
+    if(
+    document.getElementById("NIS").value == "" ||
+    document.getElementById("password1").value == ""
+    ){
+        alertbuatan("NIS dan Password harus diisi");
+    }else{
+        box.style.display = "flex";
+    }
 }
 window.onclick = function(event) {
     if (event.target == box) {
       box.style.display = "none";
+      if(document.getElementById("showhide").value != null){
+        document.getElementById("showhide").value = "none";
+      }    
     }
   }
 
+    
   function show() {
+    document.getElementById("password2").type = "text";
+  }
+  function hide(){
+    document.getElementById("password2").type = "password";
+  }
+
+  function showhide(){
     var x = document.getElementById("password2");
-    if (x.type == "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
+    if(x.type === "password"){
+        x.type = "text";
+    } else{
+        x.type = "password";
     }
   }
 
@@ -26,3 +51,11 @@ window.onclick = function(event) {
         x.type= "password";
     }
   }
+
+  function inputsalah(){
+    document.getElementById("verifbox").style.display = "none";
+    document.getElementById("password2").value = "";
+    document.getElementById("password1").value = "";
+    document.getElementById("NIS").value = "";
+    
+}
